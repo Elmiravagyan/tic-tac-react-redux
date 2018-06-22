309ef51d1b52633c1809516eb43372cb9c4413a4
Elmira <elmira@steadfast.tech>
2018-06-22 16:45:57 +0400

    Create React App

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App" style={{marginTop: "20px"}}>
+          Initial version of tic-tac-toe
+      </div>
+    );
+  }
+}
+
+export default App;
